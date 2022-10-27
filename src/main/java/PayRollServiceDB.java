7ec732ed2004deef8cb2ddd701addbab8cb621c7
Radhika <reddypallyradhika@gmail.com>
2022-10-27 12:08:06 +0530

    Added: Employee PayRoll Service MYSQLDB: Queries/Commands :UC1

--- src/main/java/PayRollServiceDB.java
+++ src/main/java/PayRollServiceDB.java
@@ -1,3 +1,22 @@
-
+/* UC1
+Ability to create a payroll
+service database
+- Using MSSQL Client use create database
+query to create a payroll_service
+database
+- Also you can see the DB created by
+using show database query
+- And go to the database created by using
+use payroll_service query
+ */
 public class PayRollServiceDB {
+    public static void main(String[] args) {
+
+    /*=====================================
+ create database  payroll_service;
+ show databases;
+ use payroll_service;
+     ======================================*/
+
+}
 }
