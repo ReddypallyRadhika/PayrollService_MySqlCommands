db11a0388e46d951ade132cb460a756a0744cd57
Radhika <reddypallyradhika@gmail.com>
2022-10-27 17:52:11 +0530

    Added: Employee PayRoll Service MYSQLDB: Queries/Commands :UC2

--- src/main/java/PayRollServiceDB.java
+++ src/main/java/PayRollServiceDB.java
@@ -1,13 +1,15 @@
-/* UC1
-Ability to create a payroll
-service database
-- Using MSSQL Client use create database
-query to create a payroll_service
-database
-- Also you can see the DB created by
-using show database query
-- And go to the database created by using
-use payroll_service query
+/* UC2
+Ability to create  employee payroll table
+in the payroll service database to
+manage employee payrolls
+- Use payroll_service database in MSSQL Client
+- Use Create Table employee_payroll Query
+to create employee payroll table with columns id,
+name, salary and start date as column. Note id is set to
+auto increment.
+- Understand the SQL data types to be used for the
+table
+- Note: SQL Queries as case-insensitive
  */
 public class PayRollServiceDB {
     public static void main(String[] args) {
@@ -16,6 +18,8 @@ public class PayRollServiceDB {
  create database  payroll_service;
  show databases;
  use payroll_service;
+ create table employee_payroll(id int auto_increment,name varchar(255) Not Null,salary double Not Null,startdate date Not Null,primary key (id));
+ select * from employee_payroll;
      ======================================*/
 
 }
